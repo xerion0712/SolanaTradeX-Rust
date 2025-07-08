86143f3d6afa2c7e25b8d096addfc029128d4959
xerion0712 <dev.xerion@gmail.com>
2025-07-08 20:05:12 +0800

    update tip_accounts

--- src/mode_fast.rs
+++ src/mode_fast.rs
@@ -41,6 +41,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "FjmZZrFvhnqqb9ThCuMVnENaM3JGVuGWNyCAxRJcFpg9",
         "6No2i3aawzHsjtThw81iq1EXPJN6rh8eSJCLaYZfKDTG",
         "A9cWowVAiHe9pJfKAj3TJiN9VpbzMUq6E4kEvf5mUT22",
+        "68Pwb4jS7eZATjDfhmTXgRJjCiZmw1L7Huy4HNpnxJ3o",
+        "4ABhJh5rZPjv63RBJBuyWzBK3g9gWMUQdTZP2kiW31V9",
+        "B2M4NG5eyZp5SBQrSdtemzk5TqVuaWGQnowGaCBt8GyM",
+        "5jA59cXMKQqZAVdtopv8q3yyw9SYfiE3vUCbt7p8MfVf",
+        "5YktoWygr1Bp9wiS1xtMtUki1PeYuuzuCF98tqwYxf61",
+        "295Avbam4qGShBYK7E9H5Ldew4B3WyJGmgmXfiWdeeyV",
+        "EDi4rSy2LZgKJX74mbLTFk4mxoTgT6F7HxxzG2HBAFyK",
+        "BnGKHAC386n4Qmv9xtpBVbRaUTKixjBe3oagkPFKtoy6",
+        "Dd7K2Fp7AtoN8xCghKDRmyqr5U169t48Tw5fEd3wT9mq",
+        "AP6qExwrbRgBAVaehg4b5xHENX815sMabtBzUzVB4v8S",
     ];
 
     let mut rng = rand::thread_rng();
